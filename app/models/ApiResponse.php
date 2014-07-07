8ab58a226f902c119d8955526ca27a35ca4558f8
Jeremy <jeremy.legros@evobili.com>
2014-07-07 16:38:26 +0800

    updated doc

--- app/models/ApiResponse.php
+++ app/models/ApiResponse.php
@@ -9,19 +9,21 @@ use Illuminate\Validation\Validator;
 use Illuminate\Support\Facades\Response as Response;
 
 /**
- *	Convenient method to format response messages
+ *	Convenient method to format Json response messages, but not only
  */
 class ApiResponse extends Response {
 
 	/**
 	 *	@param Validation $validator Illuminate\Validation\Validator instance to format errors
-	 * 	@return json String representation of the validation errors
+	 *	@param boolean $stringify Specify whether the response should be formatted in a string or as an array (default)
+	 *	@param array $headers Additional header to append to the request
+	 * 	@return json String or Array representation of the validation errors
 	 */
 	public static function validation ($validator, $stringify=false, $status='412', $headers=array() ) {
 		if ( !($validator instanceof Validator) )
 			throw new Exception('Argument is not a Validator instance ('.get_class($validator).' found).');
 
-		$response = array('result'=>'Validation passed!');
+		$response = array('validation'=>'Passed!');
 
 		if ( $validator->fails() ) {
 			$errors = $validator->messages()->toArray();
@@ -52,6 +54,7 @@ class ApiResponse extends Response {
 
 	/**
 	 *	@param url $url protocol to redirect to
+	 *	@return ApiResponse Response to client
 	 */
 	public static function makeProtocol ( $url ) {
         $response = self::make();
@@ -60,27 +63,45 @@ class ApiResponse extends Response {
     }
 
 	/**
-	 *	@param string $string Message to format
-	 *	@param code $code Error code to return
-	 * 	@return json String representation of the error message
+	 *	@param array|string $date Message to format
+	 *	@param array $headers Additional header to append to the request
+	 * 	@return ApiResponse JSON representation of the error message
 	 */
-
 	public static function errorUnauthorized( $data=array(), $headers=array() ){
 		return self::json( $data, '401', $headers );
 	}
 
+	/**
+	 *	@param array|string $date Message to format
+	 *	@param array $headers Additional header to append to the request
+	 * 	@return ApiResponse JSON representation of the error message
+	 */
 	public static function errorForbidden( $data=array(), $headers=array() ){
 		return self::json( $data, '403', $headers );
 	}
 
+	/**
+	 *	@param array|string $date Message to format
+	 *	@param array $headers Additional header to append to the request
+	 * 	@return ApiResponse JSON representation of the error message
+	 */
 	public static function errorNotFound( $data=array(), $headers=array() ){
 		return self::json( $data, '404', $headers );
 	}
 
+	/**
+	 *	@param array|string $date Message to format
+	 *	@param array $headers Additional header to append to the request
+	 * 	@return ApiResponse JSON representation of the error message
+	 */
 	public static function errorInternal( $data=array(), $headers=array() ){
 		return self::json( $data, '500', $headers );
 	}
 
+	/**
+	 *	@param array $array Message to format
+	 *	@return boolean true is associative array, false otherwise
+	 */
 	protected static function isAssocArray( $array ){
 		if ( empty($array) ) return false;
     	return (bool)count(array_filter(array_keys($array), 'is_string'));
